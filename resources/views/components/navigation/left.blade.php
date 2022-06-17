c1ecc1d0f391e1b7f7db03729556872bc28f8427
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2022-06-17 10:07:39 +0200

    added left blade for navigation bar

---
+++ resources/views/components/navigation/left.blade.php
@@ -0,0 +1,20 @@
+<ul class="navbar-nav me-auto">
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
+    </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">{{ __('About Me') }}</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">{{ __('Outdoor') }}</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">{{ __('Indoor') }}</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">{{ __('Contacts') }}</a>
+        </li>
+
+</ul>
+
+
