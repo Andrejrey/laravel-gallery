307654112aac03c1a1c77f5f3bd2cb843781672d
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2022-06-17 11:54:37 +0200

    created right and left blade for the navigation bar

--- resources/views/components/navigation/left.blade.php
+++ resources/views/components/navigation/left.blade.php
@@ -1,20 +1,21 @@
 <ul class="navbar-nav me-auto">
+    @auth
     <li class="nav-item">
         <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
     </li>
         <li class="nav-item">
-            <a class="nav-link" href="">{{ __('About Me') }}</a>
+            <a class="nav-link" href="{{ route('about_me.index') }}">{{ __('About Me') }}</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="">{{ __('Outdoor') }}</a>
+            <a class="nav-link" href="{{ route('outdoor.index') }}">{{ __('Outdoor') }}</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="">{{ __('Indoor') }}</a>
+            <a class="nav-link" href="{{ route('indoor.index') }}">{{ __('Indoor') }}</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="">{{ __('Contacts') }}</a>
+            <a class="nav-link" href="{{ route('contact.index') }}">{{ __('Contacts') }}</a>
         </li>
-
+    @endauth
 </ul>
 
 
