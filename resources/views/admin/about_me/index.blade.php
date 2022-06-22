edd811001f797b282fbde878bd4b7ffeaf805c54
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2022-06-22 15:43:05 +0200

    updated css

--- resources/views/admin/about_me/index.blade.php
+++ resources/views/admin/about_me/index.blade.php
@@ -1,27 +1,33 @@
 @extends('layouts.app')
 @section('content')
-<h1>About Me</h1>
-
-<div class="col-md-12 text-end mt-4" style="margin-bottom: 10px;">
-    <a class="btn btn-primary" href="{{ route('about_me.create') }}">
-        {{ __('Add Biography') }}
-    </a>
-</div>
-<table class="table table-striped">
-    @foreach($aboutMe as $biography)
-        <tr>
-            <td>{{$biography->title}}</td>
-            <td><img height="100" src="/storage/about_me/{{$biography->img}}"></td>
-            <td>
-                <x-form action="{{route('about_me.destroy', $biography)}}" method="post">
-                    <a class="btn btn-primary" href="{{ route('about_me.edit', $biography) }}">{{ __('Edit') }}</a>
-                    @csrf
-                    @method('delete')
-                    <x-form-submit type="submit" class="btn btn-danger delsoft">{{ __('Delete') }}</x-form-submit>
-                </x-form>
-            </td>
-        </tr>
-    @endforeach
-</table>
+    <div class="container">
+        <h1>About Me</h1>
+        <div class="col-md-12 mt-4" style="margin-bottom: 10px;">
+            <a class="btn btn-primary" href="{{ route('about_me.create') }}">
+                {{ __('Add Biography') }}
+            </a>
+        </div>
+        <table class="table table-striped">
+            <tr>
+                <td>Image:</td>
+                <td>Title:</td>
+                <td></td>
+            </tr>
+            @foreach($aboutMe as $biography)
+                <tr>
+                    <td><img height="100" src="/storage/about_me/{{$biography->img}}"></td>
+                    <td>{{$biography->title}}</td>
+                    <td>
+                        <x-form class="text-end" action="{{route('about_me.destroy', $biography)}}" method="post">
+                            <a class="btn btn-primary" href="{{ route('about_me.edit', $biography) }}">{{ __('Edit') }}</a>
+                            @csrf
+                            @method('delete')
+                            <x-form-submit type="submit" class="btn btn-danger delsoft">{{ __('Delete') }}</x-form-submit>
+                        </x-form>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
 @endsection
 
--- resources/views/admin/contacts/index.blade.php
+++ resources/views/admin/contacts/index.blade.php
@@ -1,26 +1,33 @@
 @extends('layouts.app')
 @section('content')
-<h1>Contact</h1>
-<div class="col-md-12 text-end mt-4" style="margin-bottom: 10px;">
-    <a class="btn btn-primary" href="{{ route('contact.create') }}">
-        {{ __('Add Contact') }}
-    </a>
-</div>
-<table class="table ta
-<table class="table table-striped">
-    @foreach($contacts as $contact)
-    <tr>
-        <td>{{$contact->subject}}</td>
-        <td>
-            <x-form action="{{route('contact.destroy', $contact)}}" method="post">
-                <a class="btn btn-primary" href="{{ route('contact.edit', $contact) }}">{{ __('Edit') }}</a>
-                @csrf
-                @method('delete')
-                <x-form-submit type="submit" class="btn btn-danger delsoft">{{ __('Delete') }}</x-form-submit>
-            </x-form>
-        </td>
-    </tr>
-    @endforeach
-</table>
+    <div class="container">
+        <h1>Contact</h1>
+        <div class="col-md-12 mt-4" style="margin-bottom: 10px;">
+            <a class="btn btn-primary" href="{{ route('contact.create') }}">
+                {{ __('Add Contact') }}
+            </a>
+        </div>
+        <table class="table table-striped">
+        <tr>
+            <td>Subject:</td>
+            <td>E-mail:</td>
+            <td></td>
+        </tr>
+            @foreach($contacts as $contact)
+            <tr>
+                <td>{{$contact->subject}}</td>
+                <td>{{$contact->email}}</td>
+                <td>
+                    <x-form class="text-end" action="{{route('contact.destroy', $contact)}}" method="post">
+                        <a class="btn btn-primary" href="{{ route('contact.edit', $contact) }}">{{ __('Edit') }}</a>
+                        @csrf
+                        @method('delete')
+                        <x-form-submit type="submit" class="btn btn-danger delsoft">{{ __('Delete') }}</x-form-submit>
+                    </x-form>
+                </td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
 @endsection
 
