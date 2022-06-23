a6fd12c09cb3f33bf3e6f7a0c52af0f07c5ec166
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2022-06-23 15:17:02 +0200

    add styling to index blades

--- resources/views/admin/contacts/index.blade.php
+++ resources/views/admin/contacts/index.blade.php
@@ -1,33 +1,42 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <h1>Contact</h1>
-        <div class="col-md-12 mt-4" style="margin-bottom: 10px;">
-            <a class="btn btn-primary" href="{{ route('contact.create') }}">
-                {{ __('Add Contact') }}
-            </a>
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <h6 class="card-header bg-dark bg-gradient text-white">Contacts</h6>
+                    <div class="col-md-12 mt-4" style="margin-bottom: 10px;">
+                        <a class="btn btn-success" href="{{ route('contact.create') }}">
+                            {{ __('Add Contact') }}
+                        </a>
+                    </div>
+                    {{ $contacts->links() }}
+                        <table class="table table-striped">
+                        <tr class="bg-dark bg-gradient">
+                            <td class="text-white">Subject:</td>
+                            <td class="text-white">E-mail:</td>
+                            <td></td>
+                        </tr>
+                            @foreach($contacts as $contact)
+                            <tr class="align-middle">
+                                <td>{{$contact->subject}}</td>
+                                <td>{{$contact->email}}</td>
+                                <td>
+                                    <x-form class="text-end" action="{{route('contact.destroy', $contact)}}" method="post">
+                                        <a class="btn btn-primary" href="{{ route('contact.edit', $contact) }}">{{ __('Edit') }}</a>
+                                        @csrf
+                                        @method('delete')
+                                        <x-form-submit type="submit" class="btn btn-danger delsoft">{{ __('Delete') }}</x-form-submit>
+                                    </x-form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            </div>
+        {{ $contacts->links() }}
         </div>
-        <table class="table table-striped">
-        <tr class="bg-dark bg-gradient">
-            <td class="text-white">Subject:</td>
-            <td class="text-white">E-mail:</td>
-            <td></td>
-        </tr>
-            @foreach($contacts as $contact)
-            <tr class="align-middle">
-                <td>{{$contact->subject}}</td>
-                <td>{{$contact->email}}</td>
-                <td>
-                    <x-form class="text-end" action="{{route('contact.destroy', $contact)}}" method="post">
-                        <a class="btn btn-primary" href="{{ route('contact.edit', $contact) }}">{{ __('Edit') }}</a>
-                        @csrf
-                        @method('delete')
-                        <x-form-submit type="submit" class="btn btn-danger delsoft">{{ __('Delete') }}</x-form-submit>
-                    </x-form>
-                </td>
-            </tr>
-            @endforeach
-        </table>
     </div>
 @endsection
 
--- resources/views/admin/outdoors/index.blade.php
+++ resources/views/admin/outdoors/index.blade.php
@@ -1,34 +1,43 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <h1>Outdoor</h1>
-        <div class="col-md-12 mt-4" style="margin-bottom: 10px;">
-            <a class="btn btn-primary" href="{{route('outdoor.create')}}">
-                {{ __('Add Outdoor Image') }}
-            </a>
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <h6 class="card-header bg-dark bg-gradient text-white">Outdoor Images</h6>
+                    <div class="col-md-12 mt-4" style="margin-bottom: 10px;">
+                        <a class="btn btn-success" href="{{route('outdoor.create')}}">
+                            {{ __('Add Outdoor Image') }}
+                        </a>
+                    </div>
+                    {{ $outdoors->links() }}
+                        <table class="table table-striped">
+                                <tr class="bg-dark bg-gradient">
+                                    <td class="text-white">Image:</td>
+                                    <td class="text-white">Title:</td>
+                                    <td></td>
+                                </tr>
+                            @foreach($outdoors as $outdoor)
+                                <tr class="align-middle">
+                                    <td><img height="100" src="/storage/outdoor/{{$outdoor->filename}}"></td>
+                                    <td>{{$outdoor->title}}</td>
+                                    <td>
+                                        <x-form class="text-end" action="{{route('outdoor.destroy', $outdoor)}}" method="post">
+                                            <a class="btn btn-primary" href="{{ route('outdoor.edit', $outdoor)}}"><i class="bi bi-pencil-square"></i> <span>{{ __('Edit') }}</span></a>
+                                            @csrf
+                                            @method('delete')
+                                            <x-form-submit type="submit" class="btn btn-danger delsoft">{{ __('Delete') }}</x-form-submit>
+                                        </x-form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            </div>
+        {{ $outdoors->links() }}
         </div>
-        <table class="table table-striped">
-                <tr class="bg-dark bg-gradient">
-                    <td class="text-white">Image:</td>
-                    <td class="text-white">Title:</td>
-                    <td></td>
-                </tr>
-            @foreach($outdoors as $outdoor)
-                <tr class="align-middle">
-                    <td><img height="100" src="/storage/outdoor/{{$outdoor->filename}}"></td>
-                    <td>{{$outdoor->title}}</td>
-                    <td>
-                        <x-form class="text-end" action="{{route('outdoor.destroy', $outdoor)}}" method="post">
-                            <a class="btn btn-primary" href="{{ route('outdoor.edit', $outdoor)}}">{{ __('Edit') }}</a>
-                            @csrf
-                            @method('delete')
-                            <x-form-submit type="submit" class="btn btn-danger delsoft">{{ __('Delete') }}</x-form-submit>
-                        </x-form>
-                    </td>
-                </tr>
-            @endforeach
-        </table>
     </div>
-    {{ $outdoors->links() }}
+
 @endsection
 
