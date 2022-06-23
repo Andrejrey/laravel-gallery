a0fc06603de37d77c7af2f41cec7d6f29f617ad7
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2022-06-23 15:11:42 +0200

    add styling to create blades

--- resources/views/admin/contacts/create.blade.php
+++ resources/views/admin/contacts/create.blade.php
@@ -1,13 +1,20 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Create Contact</h1>
-
-    <x-form method="post" action="{{ route('contact.store') }}" >
-        <x-form-input type="text" class="mb-3" name="subject" label="{{ __('Subject') }}" />
-        <x-form-input type="text" class="mb-3" name="email" label="{{ __('Email') }}" />
-        <x-form-textarea class="mb-3" name="message" label="{{ __('Message') }}" />
-        <x-form-submit class="mt-3">{{ __('Create') }}</x-form-submit>
-    </x-form>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <h6 class="card-header bg-dark bg-gradient text-white">Create Contact</h6>
+                    <x-form method="post" action="{{ route('contact.store') }}" >
+                        <x-form-input type="text" class="mb-3" name="subject" label="{{ __('Subject') }}" />
+                        <x-form-input type="text" class="mb-3" name="email" label="{{ __('Email') }}" />
+                        <x-form-textarea class="mb-3" name="message" label="{{ __('Message') }}" />
+                        <x-form-submit class="mt-3 btn-success">{{ __('Create') }}</x-form-submit>
+                    </x-form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 
