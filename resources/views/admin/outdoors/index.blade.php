af7e1428ff05ce8e140b1605b0640034d3251e86
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-06-23 15:54:27 +0200

    outdoor images add responsive button icons

--- resources/views/admin/outdoors/index.blade.php
+++ resources/views/admin/outdoors/index.blade.php
@@ -7,7 +7,8 @@
                     <h6 class="card-header bg-dark bg-gradient text-white">Outdoor Images</h6>
                     <div class="col-md-12 mt-4" style="margin-bottom: 10px;">
                         <a class="btn btn-success" href="{{route('outdoor.create')}}">
-                            {{ __('Add Outdoor Image') }}
+                            <i class="bi bi-file-earmark-plus-fill"></i>
+                            <span class="d-none d-md-inline">{{ __('Add Outdoor Image') }}</span>
                         </a>
                     </div>
                     {{ $outdoors->links() }}
@@ -23,10 +24,13 @@
                                     <td>{{$outdoor->title}}</td>
                                     <td>
                                         <x-form class="text-end" action="{{route('outdoor.destroy', $outdoor)}}" method="post">
-                                            <a class="btn btn-primary" href="{{ route('outdoor.edit', $outdoor)}}"><i class="bi bi-pencil-square"></i> <span>{{ __('Edit') }}</span></a>
-                                            @csrf
+                                            <a class="btn btn-primary" href="{{ route('outdoor.edit', $outdoor)}}">
+                                                <i class="bi bi-pencil-square"></i>
+                                                <span class="d-none d-md-inline">{{ __('Edit') }}</span></a>
                                             @method('delete')
-                                            <x-form-submit type="submit" class="btn btn-danger delsoft">{{ __('Delete') }}</x-form-submit>
+                                            <x-form-submit type="submit" class="btn btn-danger delsoft">
+                                                <i class="bi bi-trash-fill"></i>
+                                                <span class="d-none d-md-inline">{{ __('Delete') }}</span></x-form-submit>
                                         </x-form>
                                     </td>
                                 </tr>
