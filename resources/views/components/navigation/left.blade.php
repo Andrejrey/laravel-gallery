3c8912e051b4378fe9125c4a25af820afedc1094
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2022-06-23 15:17:48 +0200

    add styling to left blade

--- resources/views/components/navigation/left.blade.php
+++ resources/views/components/navigation/left.blade.php
@@ -1,19 +1,19 @@
-<ul class="navbar-nav me-auto text-white">
+<ul class="navbar-nav me-auto">
     @auth
     <li class="nav-item">
-        <a class="nav-link text-white" href="{{ route('home') }}">{{ __('Home') }}</a>
+        <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
     </li>
         <li class="nav-item">
-            <a class="nav-link text-white" href="{{ route('about_me.index') }}">{{ __('About Me') }}</a>
+            <a class="nav-link" href="{{ route('about_me.index') }}">{{ __('About Me') }}</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link text-white" href="{{ route('outdoor.index') }}">{{ __('Outdoor') }}</a>
+            <a class="nav-link" href="{{ route('outdoor.index') }}">{{ __('Outdoor') }}</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link text-white" href="{{ route('indoor.index') }}">{{ __('Indoor') }}</a>
+            <a class="nav-link" href="{{ route('indoor.index') }}">{{ __('Indoor') }}</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link text-white" href="{{ route('contact.index') }}">{{ __('Contacts') }}</a>
+            <a class="nav-link" href="{{ route('contact.index') }}">{{ __('Contacts') }}</a>
         </li>
     @endauth
 </ul>
